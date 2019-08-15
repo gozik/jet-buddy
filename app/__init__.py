bb4bf8c6903e0a572dcc064ed0c5461f9c0c866d
gozik <locoed.fan@gmail.com>
2019-08-16 01:26:20 +0300

    Upload file at index page.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-password'
+app.config['UPLOAD_FOLDER'] = os.path.join('.', 'uploads')
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+import os
+from flask import render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = set(['txt', 'csv', 'xlsx'])
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file', filename=filename))
+    return render_template('index.html')
