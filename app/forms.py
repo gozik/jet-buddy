309de7feb4bd1d1f119f1b2603f455c56fc3e974
gozik <locoed.fan@gmail.com>
2019-08-18 16:44:22 +0300

    Move file upload from index. Adjust navigation

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import DecimalField, StringField, DateTimeField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, AnyOf
 
 class TransactionForm(FlaskForm):
+    categories = ['food', 'drinks', 'rent']
+
     value = DecimalField('value', validators=[DataRequired()])
-    category = StringField('category', validators=[DataRequired()])
+    category = StringField('category', validators=[DataRequired(), AnyOf(categories)])
     date = DateTimeField('DateTime', validators=[DataRequired()])
     submit = SubmitField('Add')
--- app/routes.py
+++ app/routes.py
@@ -10,14 +10,8 @@ def allowed_file(filename):
     ALLOWED_EXTENSIONS = set(['txt', 'csv', 'xlsx'])
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload_file', filename=filename))
+@app.route('/')
+def index():
     return render_template('index.html')
 
 @app.route('/transactions', methods=['GET', 'POST'])
@@ -31,3 +25,13 @@ def transactions():
         db.session.commit()
         return redirect(url_for('transactions'))
     return render_template('transactions.html', transactions=Transaction.query.all(), form=form)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file', filename=filename))
+    return render_template('upload.html')
